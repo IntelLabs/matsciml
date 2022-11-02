7c708ef7434fd8feb6fe18c8fbdaa8c1a2462112
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2022-11-02 16:28:12 -0700

    example: implemented a simple leaderboard example

---
+++ examples/leaderboard_format_example.py
@@ -0,0 +1,30 @@
+import pytorch_lightning as pl
+
+# import the callback responsible for aggregating and formatting
+# prediction results
+from ocpmodels.lightning.callbacks import LeaderboardWriter
+from ocpmodels.lightning.data_utils import IS2REDGLDataModule, is2re_devset
+from ocpmodels.models import GraphConvModel, IS2RELitModule
+
+"""
+This example walks through the bare minimum example to generate a leaderboard
+ready submission file using the IS2RE development set.
+
+Here, we use a freshly initialized `GraphConvModel`, but you
+can replace this with a `load_from_checkpoint` to grab pretrained weights.
+
+To fit with the PyTorch Lightning abstraction, we rely on the `predict` pipeline/loop
+to perform inference. The code below will run through 5 batches, then save the
+result to `inference_results/IS2RELitModule/GraphConvModel/{datetime}.npz`, in
+a format that should be evalAI ready.
+"""
+
+model = IS2RELitModule(GraphConvModel(100, 1), lr=1e-3, gamma=0.9)
+dm = IS2REDGLDataModule(predict_path=is2re_devset, batch_size=8)
+
+# limit to 5 prediction batches to demonstrate pipeline
+trainer = pl.Trainer(
+    limit_predict_batches=5, callbacks=[LeaderboardWriter("inference_results")]
+)
+# run the prediction loop
+trainer.predict(model, datamodule=dm)
