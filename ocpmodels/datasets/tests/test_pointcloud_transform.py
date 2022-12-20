dbd82e388394e61f3682a561586d7ab41ea00b0c
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2022-12-20 09:50:15 -0800

    test: added test for random sampling of subsurface nodes

--- ocpmodels/datasets/tests/test_pointcloud_transform.py
+++ ocpmodels/datasets/tests/test_pointcloud_transform.py
@@ -32,3 +32,17 @@ def test_pointcloud_s2ef_transform():
     assert all(
         [key in data.keys() for key in ["atomic_numbers", "pos", "tags", "y", "force"]]
     )
+
+
+def test_sampled_pointcloud_transform():
+    devset = S2EFDataset(
+        s2ef_devset, transforms=[t.SampledPointCloudTransform(10, False)]
+    )
+    data = next(iter(devset))
+    assert all(
+        [key in data.keys() for key in ["atomic_numbers", "pos", "tags", "y", "force"]]
+    )
+    tags = data.get("tags")
+    # ensure that the number of subsurface nodes is less than or equal
+    # the number we enforced
+    assert (tags == 0).sum() <= 10
