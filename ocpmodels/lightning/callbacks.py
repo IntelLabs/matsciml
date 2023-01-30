8cd103528d15a27f2cece63e1f6db07ad482a8e8
Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-01-30 12:51:19 -0800

    feat: implemented throughput callback
    TODO would be to actually run the callback calculation on multiple
    workers, as opposed to assuming last out the door.

--- ocpmodels/lightning/callbacks.py
+++ ocpmodels/lightning/callbacks.py
@@ -1,8 +1,15 @@
-from logging import getLogger, DEBUG
+import os
+from typing import Dict, Union, Any
+from logging import getLogger
+from time import time
+from pathlib import Path
+import json
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import Callback
+from pytorch_lightning.utilities.rank_zero import rank_zero_only
 import torch
+from torch import distributed as dist
 from torch.optim import Optimizer
 
 
@@ -51,3 +58,97 @@ class GradientCheckCallback(Callback):
                     f"Parameters with gradient norm less than {self.thres}: {gradients}"
                 )
                 self.logger.debug(msg)
+
+
+class ThroughputCallback(Callback):
+        def __init__(self, log_dir: str, batch_size: int) -> None:
+            super().__init__()
+            self.batch_size = batch_size
+            self.log_dir = log_dir
+            self.record = []
+
+        @property
+        def workers(self) -> int:
+            if dist.is_initialized():
+                return dist.get_world_size()
+            return 1
+
+        @property
+        def log_dir(self) -> Path:
+            return self._log_dir
+
+        @log_dir.setter
+        def log_dir(self, path: Union[str, Path]) -> None:
+            if isinstance(path, str):
+                path = Path(path)
+            if not path.exists():
+                os.makedirs(path, exist_ok=True)
+            self._log_dir = path
+
+        @property
+        def start_time(self) -> int:
+            return self._start_time
+
+        @start_time.setter
+        def start_time(self, value: int) -> None:
+            self._start_time = value
+
+        @property
+        def end_time(self) -> int:
+            return self._end_time
+
+        @end_time.setter
+        def end_time(self, value: int) -> None:
+            self._end_time = value
+
+        @property
+        def elapsed(self) -> int:
+            return self._end_time - self._start_time
+
+        @property
+        def throughput(self) -> float:
+            return (self.batch_size * self.workers) / self.elapsed
+
+        @rank_zero_only
+        def on_train_batch_start(
+            self,
+            trainer: "pl.Trainer",
+            pl_module: "pl.LightningModule",
+            batch: Any,
+            batch_idx: int,
+        ) -> None:
+            # get time with nanosecond precision
+            self.start_time = time()
+
+        @rank_zero_only
+        def on_train_batch_end(
+            self,
+            trainer: "pl.Trainer",
+            pl_module: "pl.LightningModule",
+            outputs,
+            batch: Any,
+            batch_idx: int,
+        ) -> None:
+            self.end_time = time()
+            self.counter = batch_idx + 1
+            self.record.append(self.formatted_result)
+
+        @property
+        def formatted_result(self) -> Dict[str, Union[int, float]]:
+            result = {
+                "elapsed": self.elapsed,
+                "throughput": self.throughput,
+                "counter": self.counter,
+                "batches": self.counter * self.batch_size * self.workers,
+            }
+            return result
+
+        @rank_zero_only
+        def on_fit_end(
+            self, trainer: "pl.Trainer", pl_module: "pl.LightningModule"
+        ) -> None:
+            epoch = trainer.current_epoch
+            target = self.log_dir.joinpath(f"epoch{epoch}_throughput_measurement.json")
+            with open(target, "w+") as write_file:
+                json.dump(self.record, write_file)
+
