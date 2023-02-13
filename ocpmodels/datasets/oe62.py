b6db051cd921ed21228e5b0d385ae7c590e71a5e
Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-02-13 14:26:30 -0800

    fix: replacing from_numpy call to explicit LongTensor

--- ocpmodels/datasets/oe62.py
+++ ocpmodels/datasets/oe62.py
@@ -69,7 +69,7 @@ class OE62Dataset(DGLDataset):
         graph = DGLGraph(adj_list)
         # get coordinates, typecast to single precision
         graph.ndata["pos"] = torch.from_numpy(data.get("pos")).float()
-        graph.ndata["atomic_numbers"] = torch.from_numpy(data.get("atomic_numbers"))
+        graph.ndata["atomic_numbers"] = torch.LongTensor(data.get("atomic_numbers"))
         # get the interatomic distances as well, typecast to single precision
         graph.edata["r"] = torch.from_numpy(dist_mat[mask]).float()
         output_data["graph"] = graph
