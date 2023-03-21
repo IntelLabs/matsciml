0508559a1ea36aa8ab9d70b2694e41895da3ae21
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-03-21 12:57:14 -0700

    test: added test for API devset retrieval and serialization
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/materials_project/tests/test_api.py
+++ ocpmodels/datasets/materials_project/tests/test_api.py
@@ -1,7 +1,26 @@
 
+import pytest
+
 from ocpmodels.datasets.materials_project import MaterialsProjectRequest
 
-def test_devset():
-    dev_request = MaterialsProjectRequest.devset()
+
+@pytest.fixture
+def dev_request():
+    return MaterialsProjectRequest.devset()
+
+@pytest.mark.dependency()
+@pytest.mark.mp_api
+def test_devset(dev_request):
     data = dev_request.retrieve_data()
-    import pdb; pdb.set_trace()
+    assert len(dev_request.data) != 0
+    # pop an entry and make sure it has keys
+    datum = data.pop(0)
+    assert getattr(datum, "band_gap") is not None
+    assert hasattr(datum, "structure")
+
+
+@pytest.mark.dependency(depends=["test_devset"])
+@pytest.mark.mp_api
+def test_serialize_lmdb(dev_request):
+    data = dev_request.retrieve_data()
+    dev_request.to_lmdb("test_lmdb")
