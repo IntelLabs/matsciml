37728153a15d1e1f9d8e232d2fa5fc608437c0b9
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-03-21 18:07:10 -0700

    feat: conditional import for DGLMaterialsProjectDataset
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/materials_project/__init__.py
+++ ocpmodels/datasets/materials_project/__init__.py
@@ -1,2 +1,7 @@
+from importlib.util import find_spec
+
 from ocpmodels.datasets.materials_project.api import MaterialsProjectRequest
 from ocpmodels.datasets.materials_project.dataset import MaterialsProjectDataset
+
+if find_spec("dgl") is not None:
+    from ocpmodels.datasets.materials_project.dataset import DGLMaterialsProjectDataset
