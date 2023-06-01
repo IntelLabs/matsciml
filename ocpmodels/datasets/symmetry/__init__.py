36eaa9cdb8a79cfbb0b763fd5894299f8fe64c95
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-06-01 02:26:06 -0700

    refactor: making rowan lookup use importlib
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/symmetry/__init__.py
+++ ocpmodels/datasets/symmetry/__init__.py
@@ -1,13 +1,11 @@
 
 from warnings import warn
 from pathlib import Path
+from importlib.util import find_spec
 
-_has_rowan = False
+_has_rowan = find_spec("rowan") is not None
 
-try:
-    import rowan
-    _has_rowan = True
-except ImportError:
-    warn(f"`rowan` dependency was not installed. To use symmetry dataset, please install matsciml with `pip install './[symmetry]'`.")
+if not _has_rowan:
+    warn(f"`rowan` dependency was not installed. To generate the symmetry dataset, please install matsciml with `pip install './[symmetry]'`.")
 
 symmetry_devset = Path(__file__).parents[0].joinpath("devset")
