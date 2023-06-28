9493caddcc5fbf72712aefde4cd5dbad55b8a018
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-06-28 05:08:21 -0700

    refactor: using more generally named AbstractDataTransform
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/transforms/props.py
+++ ocpmodels/datasets/transforms/props.py
@@ -2,36 +2,10 @@ from abc import abstractmethod
 from typing import Dict, List, Union, Optional, Any
 
 import torch
-import dgl
 
-
-class AbstractGraphTransform(object):
-    @abstractmethod
-    def __call__(
-        self, data: Dict[str, Union[torch.Tensor, dgl.DGLGraph]]
-    ) -> Dict[str, Union[torch.Tensor, dgl.DGLGraph]]:
-        """
-        Call function for an abstract graph transform.
-
-        This abstract method defines the expected input and outputs;
-        we retrieve a dictionary of data, and we expect a dictionary
-        of data to come back out.
-
-        In some sense, this might not be what you would think
-        of canonically as a "transform" in that it's not operating
-        in place, but rather as modular components of the pipeline.
-
-        Parameters
-        ----------
-        data : Dict[str, Union[torch.Tensor, dgl.DGLGraph]]
-            Item from an abstract dataset
-
-        Returns
-        -------
-        Dict[str, Union[torch.Tensor, dgl.DGLGraph]]
-            Transformed item from an abstract dataset
-        """
-        raise NotImplementedError
+from ocpmodels.common import package_registry
+from ocpmodels.common.types import DataDict
+from ocpmodels.datasets.transforms.base import AbstractDataTransform
 
 
 class DistancesTransform(AbstractGraphTransform):
