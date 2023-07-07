81271696d4645c241f3251b49498d85f24ca4cb9
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-07 03:36:15 -0700

    removing unused classes from base namespace
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- ocpmodels/models/__init__.py
+++ ocpmodels/models/__init__.py
@@ -2,8 +2,6 @@
 # SPDX-License-Identifier: MIT License
 
 from ocpmodels.models.base import AbstractTask, AbstractEnergyModel
-from ocpmodels.models.base import S2EFLitModule, S2EFPointCloudModule
-from ocpmodels.models.base import IS2RELitModule, IS2REPointCloudModule
 
 from ocpmodels.models.dgl import *
 from ocpmodels.models.pyg import *
