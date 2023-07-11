7feae91587d37fbeb3c90e542269e8288021ea06
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-11 06:02:39 -0700

    test: added EGNN test
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- ocpmodels/models/dgl/tests/test_dgl_models.py
+++ ocpmodels/models/dgl/tests/test_dgl_models.py
@@ -61,3 +61,45 @@ if package_registry["dgl"]:
         assert hasattr(g_z, "grad_fn")
         # make sure every element is finite
         assert torch.isfinite(g_z).all()
+
+    @pytest.mark.dependency()
+    def test_egnn_dgl(graph):
+        egnn_kwargs = {
+            "embed_in_dim": 1,
+            "embed_hidden_dim": 32,
+            "embed_out_dim": 128,
+            "embed_depth": 5,
+            "embed_feat_dims": [128, 128, 128],
+            "embed_message_dims": [128, 128, 128],
+            "embed_position_dims": [64, 64],
+            "embed_edge_attributes_dim": 0,
+            "embed_activation": "relu",
+            "embed_residual": True,
+            "embed_normalize": True,
+            "embed_tanh": True,
+            "embed_activate_last": False,
+            "embed_k_linears": 1,
+            "embed_use_attention": False,
+            "embed_attention_norm": "sigmoid",
+            "readout": "sum",
+            "node_projection_depth": 3,
+            "node_projection_hidden_dim": 128,
+            "node_projection_activation": "relu",
+            "prediction_out_dim": 1,
+            "prediction_depth": 3,
+            "prediction_hidden_dim": 128,
+            "prediction_activation": "relu",
+            "encoder_only": True,
+        }
+
+        model = PLEGNNBackbone(**egnn_kwargs)
+        with torch.no_grad():
+            g_z = model(graph)
+        # should match embed_out_dim
+        assert g_z.shape == (1, 128)
+
+        # test with grads
+        g_z = model(graph)
+        assert hasattr(g_z, "grad_fn")
+        # make sure every element is finite
+        assert torch.isfinite(g_z).all()
