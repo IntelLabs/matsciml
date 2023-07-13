ccfe7f0b3724df89f25867c76f1b97d6e18bd54f
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-13 06:37:49 -0700

    adding sizes to ocp data sample
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/transforms/representations/pcs.py
+++ ocpmodels/datasets/transforms/representations/pcs.py
@@ -246,4 +246,5 @@ class OCPGraphToPointCloudTransform(GraphToPointCloudTransform):
             data["pos"] = node_pos
             data["src_nodes"] = src_nodes
             data["dst_nodes"] = dst_nodes
+            data["sizes"] = max(len(src_nodes), len(dst_nodes))
             data["force"] = g.ndata["force"][dst_nodes].squeeze()
