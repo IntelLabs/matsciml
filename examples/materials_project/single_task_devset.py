49956e8f8f8019b9f589679eb9ba34738391767b
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-13 04:03:54 -0700

    fix: added graph transform to devset example
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- examples/materials_project/single_task_devset.py
+++ examples/materials_project/single_task_devset.py
@@ -3,6 +3,7 @@ import pytorch_lightning as pl
 from ocpmodels.lightning.data_utils import MatSciMLDataModule
 from ocpmodels.models import GraphConvModel
 from ocpmodels.models.base import ScalarRegressionTask
+from ocpmodels.datasets.transforms import PointCloudToGraphTransform
 
 
 # configure a simple model for testing
@@ -10,7 +11,10 @@ model = GraphConvModel(100, 1, encoder_only=True)
 task = ScalarRegressionTask(model, task_keys=["band_gap"])
 
 # configure materials project from devset
-dm = MatSciMLDataModule.from_devset("MaterialsProjectDataset")
+dm = MatSciMLDataModule.from_devset(
+    "MaterialsProjectDataset",
+    dset_kwargs={"transforms": [PointCloudToGraphTransform("dgl", cutoff_dist=20.0)]},
+)
 
 # run 10 steps for funsies
 trainer = pl.Trainer(fast_dev_run=10, enable_checkpointing=False, logger=False)
