a4fde4ecfe2d33ea950d406ceaa5b71b6f4ef68f
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-13 04:02:09 -0700

    script: updating example devset script for materials project
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- examples/materials_project/single_task_devset.py
+++ examples/materials_project/single_task_devset.py
@@ -1,15 +1,18 @@
 import pytorch_lightning as pl
 
-from ocpmodels.lightning.data_utils import MaterialsProjectDataModule
+from ocpmodels.lightning.data_utils import MatSciMLDataModule
 from ocpmodels.models import GraphConvModel
 from ocpmodels.models.base import ScalarRegressionTask
 
 
+# configure a simple model for testing
 model = GraphConvModel(100, 1, encoder_only=True)
 task = ScalarRegressionTask(model, task_keys=["band_gap"])
 
-dm = MaterialsProjectDataModule.from_devset()
+# configure materials project from devset
+dm = MatSciMLDataModule.from_devset("MaterialsProjectDataset")
 
-trainer = pl.Trainer(max_steps=10, enable_checkpointing=False, logger=False)
+# run 10 steps for funsies
+trainer = pl.Trainer(fast_dev_run=10, enable_checkpointing=False, logger=False)
 
 trainer.fit(task, datamodule=dm)
