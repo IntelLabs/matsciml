da4799f8e07a55380635e11e5f97b611dc5f2801
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-12 18:32:42 -0700

    script: updated schnet example
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- examples/model_demos/schnet_dgl.py
+++ examples/model_demos/schnet_dgl.py
@@ -2,19 +2,28 @@ import pytorch_lightning as pl
 
 from ocpmodels.models import SchNet
 from ocpmodels.models.base import ScalarRegressionTask
-from ocpmodels.lightning.data_utils import IS2REDGLDataModule
+from ocpmodels.lightning.data_utils import MatSciMLDataModule
 from ocpmodels.datasets.transforms import DistancesTransform
 
 
 # construct a scalar regression task with SchNet encoder
 task = ScalarRegressionTask(
     encoder_class=SchNet,
-    encoder_kwargs={"encoder_only": True, "hidden_feats": [128, 128, 128]},
+    # kwargs to be passed into the creation of SchNet model
+    encoder_kwargs={
+        "encoder_only": True,
+        "hidden_feats": [128, 128, 128],
+        "atom_embedding_dim": 128,
+    },
+    # which keys to use as targets
     task_keys=["energy_relaxed"],
 )
+# Use IS2RE devset to test workflow
 # SchNet uses RBFs, and expects edge features corresponding to atom-atom distances
-dm = IS2REDGLDataModule.from_devset(transforms=[DistancesTransform()])
+dm = MatSciMLDataModule.from_devset(
+    "IS2REDataset", dset_kwargs={"transforms": [DistancesTransform()]}
+)
 
 # run a quick training loop
-trainer = pl.Trainer(fast_dev_run=10)
+trainer = pl.Trainer(fast_dev_run=1000)
 trainer.fit(task, datamodule=dm)
