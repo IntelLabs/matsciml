0bed584aef6b274f9bb45157f18e7154f6c41591
Carmelo Gonzales <carmelo.gonzales@intel.com>
2023-07-24 21:33:59 +0000

    adding CMDataset, example script, and updating tests

--- ocpmodels/datasets/carolina_db/__init__.py
+++ ocpmodels/datasets/carolina_db/__init__.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
 from ocpmodels.datasets.carolina_db.carolina_api import CMDRequest
+from ocpmodels.datasets.carolina_db.dataset import CMDataset
 
 carolinadb_devset = Path(__file__).parents[0].joinpath("devset")
--- ocpmodels/datasets/carolina_db/carolina_api.py
+++ ocpmodels/datasets/carolina_db/carolina_api.py
@@ -1,15 +1,17 @@
-import warnings
 import os
+import re
 import time
+import warnings
 from contextlib import suppress
 from functools import cached_property
 from typing import Dict, List, Optional, Union
 
+import lmdb
 import pandas as pd
 import requests
 import yaml
 from tqdm import tqdm
-import lmdb
+
 from ocpmodels.datasets.utils import write_lmdb_data
 
 
@@ -88,7 +90,19 @@ class CMDRequest:
         If queries fail, a text file with samples that failed will be saved so they can
         be requested again if desired.
         """
-        url = "http://www.carolinamatdb.org/static/export/cifs/{}.cif"
+
+        def request_warning(requested_data):
+            warning_message = f"Sample {n} from {self.data_dir} failed to download with: {requested_data.raise_for_status()}\n"
+            warnings.warn("warning_message")
+            with open(os.path.join(self.data_dir, f"failed.txt"), "a") as f:
+                f.write(warning_message)
+            return False
+
+        cif_url = "http://www.carolinamatdb.org/static/export/cifs/{}.cif"
+        energy_url = "http://www.carolinamatdb.org/entry/{}/"
+        energy_pattern = (
+            r'<h5>Formation Energy / Atom<\/h5>\s*<span class="value">(.*?)<\/span>'
+        )
         request_status = {}
 
         with suppress(OSError):
@@ -98,18 +112,26 @@ class CMDRequest:
             self.material_ids, desc="Total Processed: ", total=len(self.material_ids)
         ):
             time.sleep(0.001)
-            data = requests.get(url=url.format(n))
-            if data.status_code == 200:
+            data = requests.get(url=cif_url.format(n))
+            energy_data = requests.get(url=energy_url.format(n + 1))
+
+            if data.status_code != 200:
+                request_status[n] = request_warning(data)
+
+            if energy_data.status_code != 200:
+                request_status[n] = request_warning(energy_data)
+
+            if data.status_code == 200 and energy_data.status_code == 200:
                 data = data.text
+                match = re.search(energy_pattern, energy_data.text)
+                if match:
+                    energy = match.group(1).strip()
+                else:
+                    energy = None
                 with open(os.path.join(self.data_dir, f"{n}.cif"), "w") as f:
                     f.write(data)
+                    f.write(f"energy {energy}")
                 request_status[n] = True
-            else:
-                warning_message = f"Sample {n} from {self.data_dir} failed to download with code: {data.status_code}\n"
-                warnings.warn("warning_message")
-                with open(os.path.join(self.data_dir, f"failed.txt"), "a") as f:
-                    f.write(warning_message)
-                request_status[n] = False
         return request_status
 
     @cached_property
@@ -119,7 +141,27 @@ class CMDRequest:
         Returns:
             Dict[str, int]: _description_
         """
-        return dict(pd.read_csv("atomic_number_map.txt", header=None).values)
+        # fmt: off
+        an_map = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 
+              'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 
+              'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 
+              'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 
+              'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 
+              'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 
+              'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 
+              'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 
+              'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 
+              'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 
+              'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 
+              'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 
+              'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 
+              'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 
+              'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 
+              'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 
+              'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 
+              'Og': 118}
+        # fmt: on
+        return an_map
 
     def process_data(self) -> Dict:
         """Processes the raw .cif data. Grabbing any properties or attributes that are
@@ -134,14 +176,16 @@ class CMDRequest:
         for idx, file in enumerate(files):
             data = open(os.path.join(self.data_dir, file), "r").read()
             data_dict = {}
-            # files come with training blank line
+            # files sometimes come with training blank line
             lines = data.split("\n")
             if lines[-1] == "":
                 lines = lines[:-1]
             # remove the header lines:
             lines = lines[2:]
             lines = [line.strip() for line in lines]
-            # split lines into property lines, and remainder
+            # split lines into property lines, and remainder. note that properties are
+            # a mix of string, float and int values, but will all be stored as strings.
+            # converting to proper type is done in dataset (not sure if this is best)
             property_lines, lines = (
                 lines[: lines.index("loop_")],
                 lines[lines.index("loop_") + 1 :],
@@ -163,15 +207,19 @@ class CMDRequest:
             symmetries[0] = pd.to_numeric(symmetries[0])
             symmetry_dict = dict(symmetries.values)
             data_dict["symmetry_dict"] = symmetry_dict
-            # split remaining lines to get the positions and atomic numbers
-            pos_lines = lines[lines.index("_atom_site_occupancy") + 1 :]
-            pos_df = pd.DataFrame([p.split() for p in pos_lines])
-            pos = pos_df[[3, 4, 5]].to_numpy(dtype=float)
+            # split lines to get the cartesian coordinates and atomic numbers
+            cart_coords_lines = lines[lines.index("_atom_site_occupancy") + 1 : -1]
+            cart_coords_df = pd.DataFrame([p.split() for p in cart_coords_lines])
+            cart_coords = cart_coords_df[[3, 4, 5]].to_numpy(dtype=float)
             atomic_numbers = [
-                self.atomic_number_map[symbol] for symbol in pos_df[0].values
+                self.atomic_number_map[symbol] for symbol in cart_coords_df[0].values
             ]
             data_dict["atomic_numbers"] = atomic_numbers
-            data_dict["pos"] = pos
+            data_dict["cart_coords"] = cart_coords
+            data_dict["energy"] = float(lines[-1].split(maxsplit=1)[-1])
+            data_dict["formula_pretty"] = data_dict["_chemical_formula_sum"].replace(
+                " ", ""
+            )
             self.data[idx] = data_dict
 
         self.to_lmdb(os.path.dirname(self.data_dir))
@@ -215,9 +263,12 @@ class CMDRequest:
             )
 
     @classmethod
-    def devset(cls):
+    def make_devset(cls):
         kwargs = {
             "base_data_dir": "./devset",
             "material_ids": list(range(0, 100)),
         }
-        return cls(**kwargs)
+        cmd = cls(**kwargs)
+        cmd.data_dir = kwargs["base_data_dir"]
+        cmd.cmd_request()
+        cmd.process_data()
