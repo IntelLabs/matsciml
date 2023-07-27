60edb86b4abb9032b42bfb0cd83cac7ba973a830
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2023-07-27 10:04:33 -0600

    removing devset from init file

--- ocpmodels/datasets/carolina_db/__init__.py
+++ ocpmodels/datasets/carolina_db/__init__.py
@@ -2,5 +2,3 @@ from pathlib import Path
 
 from ocpmodels.datasets.carolina_db.carolina_api import CMDRequest
 from ocpmodels.datasets.carolina_db.dataset import CMDataset
-
-carolinadb_devset = Path(__file__).parents[0].joinpath("devset")
