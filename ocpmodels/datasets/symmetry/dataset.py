e3d0ccff0ecfe4ea6efe56e36d06057f06573d7a
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2023-07-31 07:47:31 -0700

    refactor: using common concatenate keys for collate
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- ocpmodels/datasets/symmetry/dataset.py
+++ ocpmodels/datasets/symmetry/dataset.py
@@ -7,61 +7,7 @@ from torch.utils.data import IterableDataset
 
 from ocpmodels.datasets.base import BaseLMDBDataset
 from ocpmodels.common.registry import registry
-from ocpmodels.datasets.utils import point_cloud_featurization
-
-
-def concatenate_keys(
-    batch: List[Dict[str, Any]], pad_keys: List[str] = []
-) -> Dict[str, Union[Dict[str, torch.Tensor], torch.Tensor]]:
-    """
-    Function for concatenating data along keys within a dictionary.
-
-    Acts as a generic concatenation function, which can also be recursively
-    applied to subdictionaries. The result is a dictionary with the same
-    structure as each sample within a batch, with the exception of
-    `target_keys` and `targets`, which are left blank for this dataset.
-
-    Parameters
-    ----------
-    batch : List[Dict[str, Any]]
-        List of samples to concatenate
-    pad_keys : List[str]
-        List of keys that are singled out to apply `pad_sequence` to.
-        This is used for atom-centered point clouds, where the number
-        of centers may not be the same between samples.
-
-    Returns
-    -------
-    Dict[str, Union[Dict[str, torch.Tensor], torch.Tensor]]
-        Concatenated data, following the same structure as each sample
-        within `batch`.
-    """
-    sample = batch[0]
-    batched_data = {}
-    for key, value in sample.items():
-        if key not in ["targets", "target_keys"]:
-            if isinstance(value, dict):
-                # apply function recursively on dictionaries
-                result = concatenate_keys([s[key] for s in batch])
-            else:
-                elements = [s[key] for s in batch]
-                if isinstance(value, torch.Tensor):
-                    # for tensors that need to be padded
-                    if key in pad_keys:
-                        result = pad_sequence(elements, batch_first=True)
-                    else:
-                        result = torch.vstack(elements)
-                # for scalar values (typically labels) pack them
-                elif isinstance(value, (float, int)):
-                    result = torch.tensor(elements)
-                # for everything else, just return a list
-                else:
-                    result = elements
-            batched_data[key] = result
-    for key in ["targets", "target_keys"]:
-        if key in sample:
-            batched_data[key] = sample[key]
-    return batched_data
+from ocpmodels.datasets.utils import point_cloud_featurization, concatenate_keys
 
 
 class OTFPointGroupDataset(IterableDataset):
