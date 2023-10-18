71a3cfb9cef934ab673725ff1adf8506572803be
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2023-10-18 10:11:16 -0700

    fixing m3gnet example to work with matgl

--- examples/model_demos/m3gnet_dgl.py
+++ examples/model_demos/m3gnet_dgl.py
@@ -1,33 +1,85 @@
+from typing import Union, List, Any, Callable, Optional, Dict
+from pathlib import Path
+import dgl
+import torch
+
 import pytorch_lightning as pl
-
-from matsciml.models.base import ScalarRegressionTask
-from matsciml.lightning.data_utils import MatSciMLDataModule
-from matsciml.datasets.transforms import PointCloudToGraphTransform
-
+from matgl.ext.pymatgen import Structure2Graph
+from matgl.graph.data import M3GNetDataset
 from matgl.models import M3GNet
+from pymatgen.core import Structure
 
+from matsciml.common.registry import registry
+from matsciml.datasets.materials_project import MaterialsProjectDataset
+from matsciml.lightning.data_utils import MatSciMLDataModule
+from matsciml.models.base import ScalarRegressionTask
+
+# fmt: off
+element_types = ['H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 
+                'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 
+                'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 
+                'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 
+                'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 
+                'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 
+                'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 
+                'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 
+                'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 
+                'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 
+                'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 
+                'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+# fmt: on
+
+
+@registry.register_dataset("M3Dataset")
+class M3Dataset(MaterialsProjectDataset):
+    def __init__(
+        self,
+        lmdb_root_path: Union[str, Path],
+        threebody_cutoff: float = 4.0,
+        graph_labels: Union[list[Union[int, float]], None] = None,
+        transforms: Optional[List[Callable[..., Any]]] = None,
+    ):
+        super().__init__(lmdb_root_path, transforms)
+        self.threebody_cutoff = threebody_cutoff
+        self.graph_labels = graph_labels
+
+    def _parse_structure(
+        self, data: Dict[str, Any], return_dict: Dict[str, Any]
+    ) -> None:
+        super()._parse_structure(data, return_dict)
+        structure: Union[None, Structure] = data.get("structure", None)
+        self.structures = [structure]
+        self.converter = Structure2Graph(
+            element_types=list(self.atomic_number_map.keys()), cutoff=5.0
+        )
+        graphs, lg, sa = M3GNetDataset.process(self)
+        return_dict["graph"] = graphs[0]
+
+
+def forward(
+    self,
+    g: dgl.DGLGraph,
+    state_attr: Union[torch.Tensor, None] = None,
+    l_g: Union[dgl.DGLGraph, None] = None,
+):
+    g = g["graph"]
+    return self.m3gnet_forward(g, state_attr, l_g)
+
+
+M3GNet.m3gnet_forward = M3GNet.forward
+M3GNet.forward = forward
 
 # construct a scalar regression task with SchNet encoder
 task = ScalarRegressionTask(
     encoder_class=M3GNet,
     encoder_kwargs={
-        "encoder_only": True,
+        "element_types": element_types,
+        "encoder_only": False,
     },
-    output_kwargs={"lazy": False, "input_dim": 64},
     task_keys=["band_gap"],
 )
-# Materials Project data needs the PointCloudToGraphTransform when using DGL.
-dm = MatSciMLDataModule.from_devset(
-    "MaterialsProjectDataset",
-    dset_kwargs={
-        "transforms": [
-            PointCloudToGraphTransform(
-                "dgl", cutoff_dist=20.0, node_keys=["pos", "atomic_numbers"]
-            )
-        ]
-    },
-    num_workers=0,
-)
+
+dm = MatSciMLDataModule.from_devset("M3Dataset", num_workers=0, batch_size=4)
 
 # run a quick training loop
 trainer = pl.Trainer(fast_dev_run=10)
--- matsciml/datasets/materials_project/dataset.py
+++ matsciml/datasets/materials_project/dataset.py
@@ -79,36 +79,6 @@ class MaterialsProjectDataset(PointCloudDataset):
             2-tuple of LMDB index and subindex
         """
         return (0, index)
-    
-    @cached_property
-    def atomic_number_map(self) -> Dict[str, int]:
-        """List of element symbols and their atomic numbers.
-
-        Returns:
-            Dict[str, int]: _description_
-        """
-        # fmt: off
-        an_map = {'H': 1, 'He': 2, 'Li': 3, 'Be': 4, 'B': 5, 'C': 6, 'N': 7, 'O': 8, 
-              'F': 9, 'Ne': 10, 'Na': 11, 'Mg': 12, 'Al': 13, 'Si': 14, 'P': 15, 
-              'S': 16, 'Cl': 17, 'Ar': 18, 'K': 19, 'Ca': 20, 'Sc': 21, 'Ti': 22, 
-              'V': 23, 'Cr': 24, 'Mn': 25, 'Fe': 26, 'Co': 27, 'Ni': 28, 'Cu': 29, 
-              'Zn': 30, 'Ga': 31, 'Ge': 32, 'As': 33, 'Se': 34, 'Br': 35, 'Kr': 36, 
-              'Rb': 37, 'Sr': 38, 'Y': 39, 'Zr': 40, 'Nb': 41, 'Mo': 42, 'Tc': 43, 
-              'Ru': 44, 'Rh': 45, 'Pd': 46, 'Ag': 47, 'Cd': 48, 'In': 49, 'Sn': 50, 
-              'Sb': 51, 'Te': 52, 'I': 53, 'Xe': 54, 'Cs': 55, 'Ba': 56, 'La': 57, 
-              'Ce': 58, 'Pr': 59, 'Nd': 60, 'Pm': 61, 'Sm': 62, 'Eu': 63, 'Gd': 64, 
-              'Tb': 65, 'Dy': 66, 'Ho': 67, 'Er': 68, 'Tm': 69, 'Yb': 70, 'Lu': 71, 
-              'Hf': 72, 'Ta': 73, 'W': 74, 'Re': 75, 'Os': 76, 'Ir': 77, 'Pt': 78, 
-              'Au': 79, 'Hg': 80, 'Tl': 81, 'Pb': 82, 'Bi': 83, 'Po': 84, 'At': 85, 
-              'Rn': 86, 'Fr': 87, 'Ra': 88, 'Ac': 89, 'Th': 90, 'Pa': 91, 'U': 92, 
-              'Np': 93, 'Pu': 94, 'Am': 95, 'Cm': 96, 'Bk': 97, 'Cf': 98, 'Es': 99, 
-              'Fm': 100, 'Md': 101, 'No': 102, 'Lr': 103, 'Rf': 104, 'Db': 105, 
-              'Sg': 106, 'Bh': 107, 'Hs': 108, 'Mt': 109, 'Ds': 110, 'Rg': 111, 
-              'Cn': 112, 'Nh': 113, 'Fl': 114, 'Mc': 115, 'Lv': 116, 'Ts': 117, 
-              'Og': 118}
-        # fmt: on
-        return an_map
-
 
     def _parse_structure(
         self, data: Dict[str, Any], return_dict: Dict[str, Any]
@@ -167,26 +137,6 @@ class MaterialsProjectDataset(PointCloudDataset):
             "lattice_params": lattice_params,
         }
         return_dict["lattice_features"] = lattice_features
-    
-        
-        # from matgl.graph.data import M3GNetDataset, MGLDataLoader, collate_fn_efs
-        # from matgl.ext.pymatgen import Structure2Graph
-
-        # self.structures = [structure]
-        # self.converter = Structure2Graph(element_types=list(self.atomic_number_map.keys()), cutoff=5.0)
-        # self.threebody_cutoff = 4.0
-        # self.graph_labels = None
-        # graphs, lg, sa = M3GNetDataset.process(self)
-        import pdb; pdb.set_trace()
-        src_id, dst_id, images, bond_dist = find_points_in_spheres(
-            coords.to(torch.double).numpy(),
-            coords.to(torch.double).numpy(),
-            r=5.0,
-            pbc=np.array([1, 1, 1], dtype=int),
-            lattice=structure.lattice.matrix,
-            tol=10e-8,
-        )
-        a = 1
 
     def _parse_symmetry(
         self, data: Dict[str, Any], return_dict: Dict[str, Any]
