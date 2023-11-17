0a0f12862939bf0174b5abbb766c18a45548a910
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2023-11-17 11:12:12 -0800

    feat: implemented embedding class structure
    This provides a regular interface for model outputs, both for
    embedding storage and also a standardized way of performing
    embedding reduction.
    
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- matsciml/common/types.py
+++ matsciml/common/types.py
@@ -1,9 +1,11 @@
-from typing import Dict, Union
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import Callable, Union
 
 import torch
 
 from matsciml.common import package_registry
-
 
 # for point clouds
 representations = [torch.Tensor]
@@ -27,7 +29,64 @@ DataType = Union[ModelingTypes]
 AbstractGraph = Union[GraphTypes]
 
 # for a dictionary look up of data
-DataDict = Dict[str, Union[float, DataType]]
+DataDict = dict[str, Union[float, DataType]]
 
 # for a dictionary of batched data
-BatchDict = Dict[str, Union[float, DataType, DataDict]]
+BatchDict = dict[str, Union[float, DataType, DataDict]]
+
+
+@dataclass
+class Embeddings:
+    """
+    Data structure that packs together embeddings from a model.
+    """
+
+    system_embedding: torch.Tensor | None = None
+    point_embedding: torch.Tensor | None = None
+    reduction: str | Callable | None = None
+    reduction_kwargs: dict[str, str | float] = {}
+
+    @property
+    def num_points(self) -> int:
+        if not isinstance(self.point_embedding, torch.Tensor):
+            raise ValueError("No point-level embeddings stored!")
+        return self.point_embedding.size(0)
+
+    @property
+    def batch_size(self) -> int:
+        if not isinstance(self.system_embedding, torch.Tensor):
+            raise ValueError(
+                "No system-level embeddings stored, can't determine batch size!",
+            )
+        return self.system_embedding.size(0)
+
+    def reduce_point_embeddings(
+        self,
+        reduction: str | Callable | None = None,
+        **reduction_kwargs,
+    ) -> torch.Tensor:
+        """
+        Perform a reduction/readout of the point-level embeddings to obtain
+        system/graph-level embeddings.
+
+        This function provides a regular interface for obtaining system-level
+        embeddings by either passing a function that functions via:
+
+        ``system_level = reduce(point_level)``
+
+        or by passing a ``str`` name of a function from ``torch`` such as ``mean``.
+        """
+        assert isinstance(
+            self.point_embedding,
+            torch.Tensor,
+        ), "No point-level embeddings stored to reduce."
+        if not reduction:
+            reduction = self.reduction
+        if isinstance(reduction, str):
+            reduction = getattr(torch, reduction)
+        if not reduction:
+            raise ValueError("No method for reduction passed.")
+        self.reduction_kwargs.update(reduction_kwargs)
+        system_embeddings = reduction(self.point_embedding, **self.reduction_kwargs)
+        self.system_embedding = system_embeddings
+        return system_embeddings
