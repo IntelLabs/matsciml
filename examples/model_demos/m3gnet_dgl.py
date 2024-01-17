2cc868422261ec50bb9e5c8faeab772f486cc437
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-01-17 15:11:45 -0800

    updating m3gnet example to set up tensor placement correctly

--- examples/model_demos/m3gnet_dgl.py
+++ examples/model_demos/m3gnet_dgl.py
@@ -1,11 +1,26 @@
 from __future__ import annotations
 
+from argparse import ArgumentParser
+
 import pytorch_lightning as pl
 
 from matsciml.datasets.utils import element_types
 from matsciml.lightning.data_utils import MatSciMLDataModule
 from matsciml.models import M3GNet
 from matsciml.models.base import ScalarRegressionTask
+
+parser = ArgumentParser()
+parser.add_argument("--gpu", action="store_true", help="Use GPU for training or not.")
+args = parser.parse_args()
+
+if args.gpu == True:
+    import torch
+
+    torch.set_default_device("cuda")
+    ACCELERATOR = "gpu"
+else:
+    ACCELERATOR = "cpu"
+
 
 # construct a scalar regression task with SchNet encoder
 task = ScalarRegressionTask(
@@ -24,5 +39,5 @@ dm = MatSciMLDataModule.from_devset(
 )
 
 # run a quick training loop
-trainer = pl.Trainer(fast_dev_run=10)
+trainer = pl.Trainer(fast_dev_run=10, accelerator=ACCELERATOR)
 trainer.fit(task, datamodule=dm)
