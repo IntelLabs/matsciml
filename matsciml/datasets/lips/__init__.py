100e0635c4c0ef73ba1121f5bc922c49770c729b
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-02-22 11:44:55 -0800

    cleaning up devset imports and definitions

--- matsciml/datasets/lips/__init__.py
+++ matsciml/datasets/lips/__init__.py
@@ -1,7 +1,3 @@
 from __future__ import annotations
 
-from pathlib import Path
-
 from matsciml.datasets.lips.dataset import LiPSDataset
-
-lips_devset = str(Path(__file__).parent.joinpath("devset"))
