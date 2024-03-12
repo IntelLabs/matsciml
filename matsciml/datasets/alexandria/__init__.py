0569d7e43293fd45fd2c46e9f8c93585a6a9e790
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2024-03-12 23:40:58 +0100

    remove unnecessary import

--- matsciml/datasets/alexandria/__init__.py
+++ matsciml/datasets/alexandria/__init__.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from pathlib import Path
 from matsciml.datasets.alexandria.api import AlexandriaRequest
 from matsciml.datasets.alexandria.dataset import AlexandriaDataset
 
