3739071c3e18c18d53ac0d29c8327e3280a6ff18
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2024-03-12 11:29:33 +0100

    removed devset path

--- matsciml/datasets/alexandria/__init__.py
+++ matsciml/datasets/alexandria/__init__.py
@@ -4,8 +4,6 @@ from pathlib import Path
 from matsciml.datasets.alexandria.api import AlexandriaRequest
 from matsciml.datasets.alexandria.dataset import AlexandriaDataset
 
-alexandria_devset = Path(__file__).parents[0].joinpath("devset")
-
 __all__ = [
     "AlexandriaDataset",
     "AlexandriaRequest",
