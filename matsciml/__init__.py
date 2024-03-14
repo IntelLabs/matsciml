5952ca75f0a225c23eb5a709f5ea7aa364f06760
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-03-14 14:43:41 -0700

    feat: ensuring intel libraries get loaded
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- matsciml/__init__.py
+++ matsciml/__init__.py
@@ -1,3 +1,11 @@
 from __future__ import annotations
 
 __version__ = "1.1.0"
+
+# determine if intel libraries are available
+from matsciml.common.packages import package_registry
+
+if package_registry["ipex"]:
+    import intel_extension_for_pytorch  # noqa: F401
+if package_registry["ccl"]:
+    import oneccl_bindings_for_pytorch  # noqa: F401
