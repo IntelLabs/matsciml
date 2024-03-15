8c85bbf3218e666bd1626a8dec619e0172a21caf
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-03-14 18:02:45 -0700

    fix: making ipex import at the beginning captured by exception
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- matsciml/__init__.py
+++ matsciml/__init__.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
-__version__ = "1.1.0"
+from logging import getLogger
 
 # determine if intel libraries are available
 from matsciml.common.packages import package_registry
 
+__version__ = "1.1.0"
+
+logger = getLogger(__file__)
+
+
 if package_registry["ipex"]:
-    import intel_extension_for_pytorch  # noqa: F401
+    try:
+        import intel_extension_for_pytorch  # noqa: F401
+    except ImportError as e:
+        logger.warning(f"Unable to load IPEX because of {e} - XPU may not function.")
 if package_registry["ccl"]:
     import oneccl_bindings_for_pytorch  # noqa: F401
