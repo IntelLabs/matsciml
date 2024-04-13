4db1fa9067d9dbaf719800def63ca1b4c68262c8
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-04-12 22:43:38 -0700

    docs: configuring pd theme
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+#
+import sphinx_theme_pd
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "Open MatSciML Toolkit"
+copyright = "2024, Intel Corporation"
+author = "Intel Corporation"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_theme_pd"
+html_theme_path = [sphinx_theme_pd.get_html_theme_path()]
+html_static_path = ["_static"]
