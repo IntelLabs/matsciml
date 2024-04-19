fdff63ae1a35220ac1053cf79330e8a6b238acbf
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-04-19 11:33:32 -0700

    refactor: cleaning up other task unit tests with fixture
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- matsciml/models/tests/test_tasks.py
+++ matsciml/models/tests/test_tasks.py
@@ -56,7 +56,7 @@ def egnn_config():
     return {"encoder_class": PLEGNNBackbone, "encoder_kwargs": model_args}
 
 
-def test_force_regression():
+def test_force_regression(egnn_config):
     devset = MatSciMLDataModule.from_devset(
         "S2EFDataset",
         dset_kwargs={
@@ -69,36 +69,7 @@ def test_force_regression():
             ],
         },
     )
-    model_args = {
-        "embed_in_dim": 128,
-        "embed_hidden_dim": 32,
-        "embed_out_dim": 128,
-        "embed_depth": 5,
-        "embed_feat_dims": [128, 128, 128],
-        "embed_message_dims": [128, 128, 128],
-        "embed_position_dims": [64, 64],
-        "embed_edge_attributes_dim": 0,
-        "embed_activation": "relu",
-        "embed_residual": True,
-        "embed_normalize": True,
-        "embed_tanh": True,
-        "embed_activate_last": False,
-        "embed_k_linears": 1,
-        "embed_use_attention": False,
-        "embed_attention_norm": "sigmoid",
-        "readout": "sum",
-        "node_projection_depth": 3,
-        "node_projection_hidden_dim": 128,
-        "node_projection_activation": "relu",
-        "prediction_out_dim": 1,
-        "prediction_depth": 3,
-        "prediction_hidden_dim": 128,
-        "prediction_activation": "relu",
-        "encoder_only": True,
-    }
-
-    model = PLEGNNBackbone(**model_args)
-    task = ForceRegressionTask(model)
+    task = ForceRegressionTask(**egnn_config)
     trainer = pl.Trainer(max_steps=5, logger=False, enable_checkpointing=False)
     trainer.fit(task, datamodule=devset)
     # make sure losses are tracked
@@ -106,7 +77,7 @@ def test_force_regression():
         assert f"train_{key}" in trainer.logged_metrics
 
 
-def test_gradfree_force_regression():
+def test_gradfree_force_regression(egnn_config):
     devset = MatSciMLDataModule.from_devset(
         "S2EFDataset",
         dset_kwargs={
@@ -119,37 +90,7 @@ def test_gradfree_force_regression():
             ],
         },
     )
-    model_args = {
-        "embed_in_dim": 128,
-        "embed_hidden_dim": 32,
-        "embed_out_dim": 128,
-        "embed_depth": 5,
-        "embed_feat_dims": [128, 128, 128],
-        "embed_message_dims": [128, 128, 128],
-        "embed_position_dims": [64, 64],
-        "embed_edge_attributes_dim": 0,
-        "embed_activation": "relu",
-        "embed_residual": True,
-        "embed_normalize": True,
-        "embed_tanh": True,
-        "embed_activate_last": False,
-        "embed_k_linears": 1,
-        "embed_use_attention": False,
-        "embed_attention_norm": "sigmoid",
-        "readout": "sum",
-        "node_projection_depth": 3,
-        "node_projection_hidden_dim": 128,
-        "node_projection_activation": "relu",
-        "prediction_out_dim": 1,
-        "prediction_depth": 3,
-        "prediction_hidden_dim": 128,
-        "prediction_activation": "relu",
-        "encoder_only": True,
-    }
-    task = GradFreeForceRegressionTask(
-        encoder_class=PLEGNNBackbone,
-        encoder_kwargs=model_args,
-    )
+    task = GradFreeForceRegressionTask(**egnn_config)
     trainer = pl.Trainer(max_steps=5, logger=False, enable_checkpointing=False)
     trainer.fit(task, datamodule=devset)
     # make sure losses are tracked
