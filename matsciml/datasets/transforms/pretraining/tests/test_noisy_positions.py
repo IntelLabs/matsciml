1ca6287abe1eb088d32879099783c41b97cfa15f
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-04-19 10:07:26 -0700

    test: added paramterized unit test for datamodule
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- matsciml/datasets/transforms/pretraining/tests/test_noisy_positions.py
+++ matsciml/datasets/transforms/pretraining/tests/test_noisy_positions.py
@@ -5,6 +5,7 @@ import torch
 from dgl import DGLGraph
 
 from matsciml.common.registry import registry
+from matsciml.lightning import MatSciMLDataModule
 from matsciml.datasets.transforms.pretraining import NoisyPositions
 from matsciml.datasets.transforms import (
     PeriodicPropertiesTransform,
@@ -57,3 +58,32 @@ def test_noisy_graph(dset_name, graph_type):
             target = graph
         assert "noisy_pos" in target
         assert torch.isfinite(target["noisy_pos"]).all()
+
+
+@pytest.mark.parametrize("dset_name", valid_dsets)
+@pytest.mark.parametrize("graph_type", ["pyg", "dgl"])
+def test_noisy_graph_datamodule(dset_name, graph_type):
+    """Test the transform on graph types with batching."""
+    dm = MatSciMLDataModule.from_devset(
+        dset_name,
+        dset_kwargs=dict(
+            transforms=[
+                NoisyPositions(),
+                PeriodicPropertiesTransform(6.0, adaptive_cutoff=True),
+                PointCloudToGraphTransform(
+                    graph_type, node_keys=["atomic_numbers", "pos", "noisy_pos"]
+                ),
+            ],
+        ),
+        batch_size=4,
+    )
+    dm.setup("fit")
+    loader = dm.train_dataloader()
+    batch = next(iter(loader))
+    graph = batch["graph"]
+    if isinstance(graph, DGLGraph):
+        target = graph.ndata
+    else:
+        target = graph
+    assert "noisy_pos" in target
+    assert torch.isfinite(target["noisy_pos"]).all()
