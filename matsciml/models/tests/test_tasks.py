4cc7db66ee4b75355048d8a9a7ec80283ae29074
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-04-19 11:31:17 -0700

    test: added unit test for node denoising task
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- matsciml/models/tests/test_tasks.py
+++ matsciml/models/tests/test_tasks.py
@@ -1,18 +1,59 @@
 from __future__ import annotations
 
+import pytest
 import pytorch_lightning as pl
 
 from matsciml.datasets.materials_project import MaterialsProjectDataset
-from matsciml.datasets.transforms import PointCloudToGraphTransform
+from matsciml.datasets.transforms import (
+    PointCloudToGraphTransform,
+    PeriodicPropertiesTransform,
+    NoisyPositions,
+)
 from matsciml.lightning.data_utils import MatSciMLDataModule
 from matsciml.models import PLEGNNBackbone
-from matsciml.models.base import ForceRegressionTask, GradFreeForceRegressionTask
+from matsciml.models.base import (
+    ForceRegressionTask,
+    GradFreeForceRegressionTask,
+    NodeDenoisingTask,
+)
 
 pl.seed_everything(2156161)
 
 
 def test_regression_devset():
     dset = MaterialsProjectDataset.from_devset()  # noqa: F841
+
+
+@pytest.fixture
+def egnn_config():
+    model_args = {
+        "embed_in_dim": 128,
+        "embed_hidden_dim": 32,
+        "embed_out_dim": 128,
+        "embed_depth": 5,
+        "embed_feat_dims": [128, 128, 128],
+        "embed_message_dims": [128, 128, 128],
+        "embed_position_dims": [64, 64],
+        "embed_edge_attributes_dim": 0,
+        "embed_activation": "relu",
+        "embed_residual": True,
+        "embed_normalize": True,
+        "embed_tanh": True,
+        "embed_activate_last": False,
+        "embed_k_linears": 1,
+        "embed_use_attention": False,
+        "embed_attention_norm": "sigmoid",
+        "readout": "sum",
+        "node_projection_depth": 3,
+        "node_projection_hidden_dim": 128,
+        "node_projection_activation": "relu",
+        "prediction_out_dim": 1,
+        "prediction_depth": 3,
+        "prediction_hidden_dim": 128,
+        "prediction_activation": "relu",
+        "encoder_only": True,
+    }
+    return {"encoder_class": PLEGNNBackbone, "encoder_kwargs": model_args}
 
 
 def test_force_regression():
@@ -113,3 +154,23 @@ def test_gradfree_force_regression():
     trainer.fit(task, datamodule=devset)
     # make sure losses are tracked
     assert "train_force" in trainer.logged_metrics
+
+
+def test_denoising_task(egnn_config):
+    dm = MatSciMLDataModule.from_devset(
+        "MaterialsProjectDataset",
+        dset_kwargs={
+            "transforms": [
+                NoisyPositions(),
+                PeriodicPropertiesTransform(6.0, True),
+                PointCloudToGraphTransform(
+                    "dgl", node_keys=["atomic_numbers", "noisy_pos", "force"]
+                ),
+            ]
+        },
+        batch_size=8,
+    )
+    task = NodeDenoisingTask(**egnn_config, task_keys=["denoise"])
+    trainer = pl.Trainer(fast_dev_run=5)
+    trainer.fit(task, datamodule=dm)
+    assert "train_denoise" in trainer.logged_metrics
