012ac9bd3edcc487f6cf59b4da2d72b0d3d3a34a
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-04-23 10:21:00 -0700

    refactor: reverting some imports back to pytorch_lightning
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

--- matsciml/lightning/ddp.py
+++ matsciml/lightning/ddp.py
@@ -13,9 +13,9 @@ from torch import nn
 from torch.nn.parallel.distributed import DistributedDataParallel
 import pytorch_lightning as pl
 from pytorch_lightning.plugins import CheckpointIO
-from lightning_fabric.plugins.environments import LightningEnvironment
 from lightning_fabric.plugins.environments.lightning import find_free_network_port
-from lightning_fabric.strategies.ddp import DDPStrategy
+from pytorch_lightning.plugins.strategies.ddp import DDPStrategy
+from pytorch_lightning.plugins.environments import LightningEnvironment
 from pytorch_lightning.plugins.precision import Precision
 from pytorch_lightning.strategies import StrategyRegistry
 
