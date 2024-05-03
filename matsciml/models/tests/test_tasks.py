97e24a7b6377248738201ea27c05b9891819abaf
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-05-03 12:31:19 -0700

    test: expanded force regression test to faenet

--- matsciml/models/tests/test_tasks.py
+++ matsciml/models/tests/test_tasks.py
@@ -10,7 +10,7 @@ from matsciml.datasets.transforms import (
     NoisyPositions,
 )
 from matsciml.lightning.data_utils import MatSciMLDataModule
-from matsciml.models import PLEGNNBackbone
+from matsciml.models import PLEGNNBackbone, FAENet
 from matsciml.models.base import (
     ForceRegressionTask,
     GradFreeForceRegressionTask,
@@ -56,6 +56,18 @@ def egnn_config():
     return {"encoder_class": PLEGNNBackbone, "encoder_kwargs": model_args}
 
 
+@pytest.fixture
+def faenet_config():
+    model_args = {
+        "average_frame_embeddings": False,
+        "pred_as_dict": False,
+        # "hidden_channels": 128,
+        # "out_dim": 128,
+        # "tag_hidden_channels": 32,
+    }
+    return {"encoder_class": FAENet, "encoder_kwargs": model_args}
+
+
 def test_force_regression(egnn_config):
     devset = MatSciMLDataModule.from_devset(
         "S2EFDataset",
@@ -70,6 +82,27 @@ def test_force_regression(egnn_config):
         },
     )
     task = ForceRegressionTask(**egnn_config)
+    trainer = pl.Trainer(max_steps=5, logger=False, enable_checkpointing=False)
+    trainer.fit(task, datamodule=devset)
+    # make sure losses are tracked
+    for key in ["energy", "force"]:
+        assert f"train_{key}" in trainer.logged_metrics
+
+
+def test_fa_force_regression(faenet_config):
+    devset = MatSciMLDataModule.from_devset(
+        "S2EFDataset",
+        dset_kwargs={
+            "transforms": [
+                PeriodicPropertiesTransform(6.0, True),
+                PointCloudToGraphTransform(
+                    "pyg",
+                    node_keys=["pos", "force", "atomic_numbers"],
+                ),
+            ],
+        },
+    )
+    task = ForceRegressionTask(**faenet_config)
     trainer = pl.Trainer(max_steps=5, logger=False, enable_checkpointing=False)
     trainer.fit(task, datamodule=devset)
     # make sure losses are tracked
