700fff85502ffe2794d4dd4560df94a3d81b0603
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-06-26 08:20:53 -0700

    fix & config: missing comma in pyproject and working sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,19 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-#
-import sphinx_theme_pd
-
 project = "Open MatSciML Toolkit"
 copyright = "2024, Intel Corporation"
 author = "Intel Corporation"
 
-extensions = ["sphinxawesome_theme.highlighting"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
 
 html_theme = "sphinxawesome_theme"
-html_theme_path = [sphinx_theme_pd.get_html_theme_path()]
 html_static_path = ["_static"]
+
+# configure napoleon docstrings
+napoleon_numpy_docstring = True
+napoleon_preprocess_types = True
