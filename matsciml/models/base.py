84950bcb1237e924ffd660cc7d4a7e1032a43720
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-07-18 15:41:08 -0700

    refactor: specifying energy and force loss calculators in forceregressiontask

--- matsciml/models/base.py
+++ matsciml/models/base.py
@@ -22,6 +22,7 @@ from matsciml.common.registry import registry
 from matsciml.common.types import AbstractGraph, BatchDict, DataDict, Embeddings
 from matsciml.models.common import OutputHead
 from matsciml.modules.normalizer import Normalizer
+from matsciml.models import losses as matsciml_losses
 
 logger = getLogger("matsciml")
 logger.setLevel("INFO")
@@ -1656,12 +1657,18 @@ class ForceRegressionTask(BaseTaskModule):
         encoder: nn.Module | None = None,
         encoder_class: type[nn.Module] | None = None,
         encoder_kwargs: dict[str, Any] | None = None,
-        loss_func: type[nn.Module] | nn.Module = nn.L1Loss,
+        loss_func: type[nn.Module] | nn.Module | None = None,
+        energy_loss_func: type[nn.Module] | nn.Module = matsciml_losses.AtomWeightedMSE,
+        force_loss_func: type[nn.Module] | nn.Module = nn.MSELoss,
         task_keys: list[str] | None = None,
         output_kwargs: dict[str, Any] = {},
         embedding_reduction_type: str = "sum",
         **kwargs,
     ) -> None:
+        if loss_func is not None:
+            logger.warning(
+                "loss_func is now ignored; please set energy/force_loss_func instead."
+            )
         super().__init__(
             encoder,
             encoder_class,
@@ -1672,7 +1679,15 @@ class ForceRegressionTask(BaseTaskModule):
             embedding_reduction_type=embedding_reduction_type,
             **kwargs,
         )
-        self.save_hyperparameters(ignore=["encoder", "loss_func"])
+        if isinstance(energy_loss_func, type):
+            energy_loss_func = energy_loss_func()
+        if isinstance(force_loss_func, type):
+            force_loss_func = force_loss_func()
+        self.energy_loss_func = energy_loss_func
+        self.force_loss_func = force_loss_func
+        self.save_hyperparameters(
+            ignore=["encoder", "loss_func", "energy_loss_func", "force_loss_func"]
+        )
         # have to enable double backprop
         self.automatic_optimization = False
 
