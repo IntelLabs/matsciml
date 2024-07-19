ba6930667baa481272b18b8ec22da95cf53ed777
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-07-19 09:40:02 -0700

    refactor: adding exception handling for L1 weighted loss as well

--- matsciml/models/losses.py
+++ matsciml/models/losses.py
@@ -19,6 +19,12 @@ class AtomWeightedL1(nn.Module):
         target: torch.Tensor,
         atoms_per_graph: torch.Tensor,
     ) -> torch.Tensor:
+        if atoms_per_graph.size(0) != target.size(0):
+            raise RuntimeError(
+                "Dimensions for atom-weighted loss do not match:"
+                f" expected atoms_per_graph to have {target.size(0)} elements; got {atoms_per_graph.size(0)}."
+                "This loss is intended to be applied to scalar targets only."
+            )
         # check to make sure we are broad casting correctly
         if (input.ndim != target.ndim) and target.size(-1) == 1:
             input.unsqueeze_(-1)
