fc7437901fe7eb34104a91fce23d5af4b140be0c
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-08-05 16:50:49 -0700

    feat: adding mptraj to datasets namespace
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- matsciml/datasets/materials_project/__init__.py
+++ matsciml/datasets/materials_project/__init__.py
@@ -3,7 +3,16 @@ from __future__ import annotations
 from importlib.util import find_spec
 
 from matsciml.datasets.materials_project.api import MaterialsProjectRequest
-from matsciml.datasets.materials_project.dataset import MaterialsProjectDataset
+from matsciml.datasets.materials_project.dataset import (
+    MaterialsProjectDataset,
+    MaterialsTrajectoryDataset,
+)
+
+__all__ = [
+    "MaterialsProjectDataset",
+    "MaterialsTrajectoryDataset",
+    "MaterialsProjectRequest",
+]
 
 if find_spec("torch_geometric") is not None:
     from matsciml.datasets.materials_project.dataset import (
@@ -12,3 +21,6 @@ if find_spec("torch_geometric") is not None:
         PyGMaterialsProjectDataset,
     )
 
+    __all__.extend(
+        ["CdvaeLMDBDataset", "PyGCdvaeDataset", "PyGMaterialsProjectDataset"]
+    )
