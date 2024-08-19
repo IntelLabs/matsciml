0982c59e000d455da96fd3396ca04f3bea85fc31
lory-w <xiaoxiao.wang@intel.com>
2024-08-19 14:57:12 -0700

    ruff

--- matsciml/models/pyg/mace/wrapper/model.py
+++ matsciml/models/pyg/mace/wrapper/model.py
@@ -152,6 +152,7 @@ class MACEWrapper(AbstractPyGModel):
         # hacky way of letting MACE work for single graph
         if "batch" not in graph:
             from torch_geometric.data import Batch
+            
             graph = Batch.from_data_list([graph])
         assert hasattr(graph, "ptr"), "Graph is missing the `ptr` attribute!"
         # the name of these keys matches up with our `_forward`, and
