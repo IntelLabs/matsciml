df8cc64e2b41c72790047cb287d3e43e317baa15
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-09-09 09:51:51 -0700

    fix: correcting intended behavior for SAM skipping

--- matsciml/lightning/callbacks.py
+++ matsciml/lightning/callbacks.py
@@ -840,13 +840,13 @@ class SAM(Callback):
         current_epoch = trainer.current_epoch
         # by default we start SAM, and toggle off if conditions met
         start_sam = True
-        if self.skip_epoch_count and self.skip_epoch_count < current_epoch:
+        if self.skip_epoch_count and not self.skip_epoch_count < current_epoch:
             start_sam = False
             if self.logger:
                 self.logger.info(
-                    "Required number of steps not met; not running SAM yet."
+                    "Required number of epochs not met; not running SAM yet."
                 )
-        if self.skip_step_count and self.skip_step_count < current_step:
+        if self.skip_step_count and not self.skip_step_count < current_step:
             start_sam = False
             if self.logger:
                 self.logger.info(
