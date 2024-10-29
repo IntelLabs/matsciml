3c9e2598157e11be45d235229c61f314fe255e3e
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-10-29 10:53:43 -0700

    refactor: making pyg model imports dependent on package registry
    Signed-off-by: Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>

--- matsciml/models/pyg/__init__.py
+++ matsciml/models/pyg/__init__.py
@@ -4,6 +4,7 @@ Copyright (c) Facebook, Inc. and its affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
+
 from __future__ import annotations
 
 from matsciml.common.packages import package_registry
@@ -16,10 +17,20 @@ else:
 # load models if we have PyG installed
 if _has_pyg:
     from matsciml.models.pyg.cgcnn import CGCNN
-    from matsciml.models.pyg.dimenet import DimeNetWrap
-    from matsciml.models.pyg.dimenet_plus_plus import DimeNetPlusPlusWrap
     from matsciml.models.pyg.egnn import EGNN
     from matsciml.models.pyg.faenet import FAENet
-    from matsciml.models.pyg.forcenet import ForceNet
     from matsciml.models.pyg.mace import MACE, ScaleShiftMACE
-    from matsciml.models.pyg.schnet import SchNetWrap
+
+    __all__ = ["CGCNN", "EGNN", "FAENet", "MACE", "ScaleShiftMACE"]
+
+    # these packages need additional pyg dependencies
+    if package_registry["torch_sparse"] and package_registry["torch_scatter"]:
+        from matsciml.models.pyg.dimenet import DimeNetWrap
+        from matsciml.models.pyg.dimenet_plus_plus import DimeNetPlusPlusWrap
+
+        __all__.extend(["DimeNetWrap", "DimeNetPlusPlusWrap"])
+    if package_registry["torch_scatter"]:
+        from matsciml.models.pyg.forcenet import ForceNet
+        from matsciml.models.pyg.schnet import SchNetWrap
+
+        __all__.extend(["ForceNet", "SchNetWrap"])
