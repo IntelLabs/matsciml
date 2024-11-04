e3fe7a298125a874f1e22dafb998f5b646ac4347
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-11-04 15:59:44 -0800

    feat: implemented generic matsciml dataset
    Signed-off-by: Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>

---
+++ matsciml/datasets/generic.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+from functools import cache
+from os import PathLike
+from pathlib import Path
+from typing import Callable
+from logging import getLogger
+
+import h5py
+from torch.utils.data import Dataset
+from lightning import pytorch as pl
+
+from matsciml.datasets.schema import DatasetSchema, DataSampleSchema
+
+
+logger = getLogger("matsciml.datasets.MatSciMLDataset")
+
+
+class MatSciMLDataset(Dataset):
+    def __init__(self, filepath: PathLike, transforms: list[Callable] | None = None):
+        if not isinstance(filepath, Path):
+            filepath = Path(filepath)
+        self.filepath = filepath
+        self.transforms = transforms
+
+    @property
+    def metadata(self) -> DatasetSchema:
+        meta_target = self.filepath.parent.joinpath("metadata.json")
+        if not meta_target.exists():
+            raise RuntimeError("No metadata for dataset.")
+        return DatasetSchema.parse_file(meta_target)
+
+    @property
+    def data(self) -> h5py.File:
+        return h5py.File(str(self.filepath.absolute()), mode="r")
+
+    @cache
+    def __len__(self) -> int:
+        with self.data as h5_data:
+            return len(h5_data.keys())
+
+    def __getitem__(self, index: int):
+        index = str(index)
+        with self.data as h5_data:
+            try:
+                sample_group = h5_data[index]
+            except KeyError as e:
+                raise KeyError(f"Data sample {index} missing from dataset.") from e
+            sample_data = {}
+            for key, value in sample_group.items():
+                sample_data[key] = value
+            sample = DataSampleSchema(**sample_data)
+            # now try to apply transforms
+            if self.transforms:
+                for transform in self.transforms:
+                    try:
+                        sample = transform(sample)
+                    except Exception as e:
+                        raise RuntimeError(
+                            f"Unable to apply {transform} on sample at index {index}."
+                        ) from e
+            return sample
+
+
+class MatSciMLDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        filepath: PathLike,
+        transforms: list[Callable] | None = None,
+        **loader_kwargs,
+    ):
+        if not isinstance(filepath, Path):
+            filepath = Path(filepath)
+        if not filepath.is_dir():
+            raise RuntimeError(f"Expected filepath to be a directory; got {filepath}")
+        self.metadata = filepath.joinpath("metadata.json")
+        self.filepath = filepath
+        self.transforms = transforms
+        self.loader_kwargs = loader_kwargs
+
+    @property
+    def metadata(self) -> DatasetSchema:
+        return self._metadata
+
+    @metadata.setter
+    def metadata(self, filepath: Path) -> None:
+        if not filepath.exists():
+            raise RuntimeError(
+                "No metadata found in target directory. Expected a metadata.json to exist."
+            )
+        metadata = DatasetSchema.parse_file(filepath)
+        self._metadata = metadata
+
+    @property
+    def h5_files(self) -> dict[str, Path]:
+        return self._h5_files
+
+    @h5_files.setter
+    def h5_files(self, root_dir: Path) -> None:
+        h5_files = {}
+        for prefix in ["train", "test", "validation", "predict"]:
+            h5_file = root_dir.joinpath(prefix).with_suffix(".h5")
+            if h5_file.exists():
+                h5_files[prefix] = h5_file.absolute()
+                logger.debug(f"Found {h5_file} data.")
+        if len(h5_files) == 0:
+            raise RuntimeError("No .h5 files found in target directory.")
+        self._h5_files = h5_files
+
+    def setup(self, stage: str):
+        # check and set the available HDF5 data files
+        self.h5_files = self.filepath
+        self.datasets = {
+            key: MatSciMLDataset(path, self.transforms)
+            for key, path in self.h5_files.items()
+        }
+        if stage == "fit":
+            assert "train" in self.datasets, "No training split available!"
+        if stage == "predict":
+            assert "predict" in self.datasets, "No predict split available!"
