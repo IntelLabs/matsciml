9b608e5235cba2865d40396071a130f3b60bb40c
Lee, Kin Long Kelvin <kin.long.kelvin.lee@intel.com>
2024-11-08 14:37:24 -0800

    test: added unit tests for type validation

--- matsciml/common/tests/test_types.py
+++ matsciml/common/tests/test_types.py
@@ -1,8 +1,24 @@
 from __future__ import annotations
 
+import torch
+import pytest
+from pydantic import ValidationError
+
 from matsciml.common import types
 
 
 def test_output_validation():
     """Simple unit test to make sure a minimal configuration works."""
     types.ModelOutput(batch_size=16, embeddings=None, node_energies=None)
+
+
+def test_invalid_embeddings():
+    """Type invalid embeddings"""
+    with pytest.raises(ValidationError):
+        types.ModelOutput(batch_size=8, embeddings="aggaga")
+
+
+def test_incorrect_force_shape():
+    """This passes a force tensor with too many dimensions"""
+    with pytest.raises(ValidationError):
+        types.ModelOutput(batch_size=8, forces=torch.rand(32, 4, 3))
