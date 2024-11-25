7257eb585a6351bb9c5506ed9e376ad165b8c05d
Kin Long Kelvin Lee <kin.long.kelvin.lee@intel.com>
2024-11-25 12:55:26 -0800

    refactor: updating __getitem__ call to use target schema

--- matsciml/datasets/generic.py
+++ matsciml/datasets/generic.py
@@ -151,9 +151,12 @@ class MatSciMLDataset(Dataset):
             sample_data = {}
             for key, value in sample_group.items():
                 sample_data[key] = value
-            for key in self.metadata.target_keys:
-                if key not in sample_data:
-                    raise KeyError(f"Expected {key} in data sample but not found.")
+            # validate expected data
+            for target in self.metadata.targets:
+                if target.name not in sample_data:
+                    raise KeyError(
+                        f"Expected {target.name} in data sample but not found."
+                    )
             sample = DataSampleSchema(**sample_data)
             # now try to apply transforms
             if self.transforms:
